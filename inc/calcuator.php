bbeeb279d5aa7f8d52a318c0e0db219fd4592fd7
MD Habibur Rahman <83889714+HAbib524239@users.noreply.github.com>
2021-06-24 15:15:33 +0600

    php_calculatot
    this is php

---
+++ inc/calcuator.php
@@ -0,0 +1,83 @@
+ 
+ 
+ 
+ 
+ 
+        $numberone = $_POST['num1'];
+		$numbertwo = $_POST['num2'];
+		$numberthre= $_POST['num3'];
+		$numberfour= $_POST['num4'];
+		$numberfive= $_POST['num5'];
+		$numbersix = $_POST['num6'];
+		$numberseven= $_POST['num7'];
+		$numbereight= $_POST['num8'];
+		$numbernine = $_POST['num9'];
+		$numberzero=$_POST['num0'];
+		$addition = $_POST['add'];
+		$subtraction = $_POST['sub'];
+		$multipacation = $_POST['multi'];
+		$devided = $_POST['devide'];
+		$bagses = $_POST['bagses']; 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	//   if(isset($_POST['num1'])or isset($_POST['num2']) or isset($_POST['num3']) or isset ($_POST['num4'])
+	// or isset($_POST['num5']) or isset($_POST['num6']) or isset($_POST['num7']) or isset($_POST['num8'])	
+     // or isset ($_POST['num9']) or isset($_POST['num0']) or isset($_POST['add']) or isset($_POST['sub'])
+	 // or isset($_POST['multi']) or isset($_POST['devide']) or isset($_POST['bagses'])){ 
+	 
+	 
+	    // $numberone = $_POST['num1'];
+		// $numbertwo = $_POST['num2'];
+		// $numberthre= $_POST['num3'];
+		// $numberfour= $_POST['num4'];
+		// $numberfive= $_POST['num5'];
+		// $numbersix = $_POST['num6'];
+		// $numberseven= $_POST['num7'];
+		// $numbereight= $_POST['num8'];
+		// $numbernine = $_POST['num9'];
+		// $numberzero=$_POST['num0'];
+		// $addition = $_POST['add'];
+		// $subtraction = $_POST['sub'];
+		// $multipacation = $_POST['multi'];
+		// $devided = $_POST['devide'];
+		// $bagses = $_POST['bagses']; 
+	 
+	 // }  
+
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+ 
